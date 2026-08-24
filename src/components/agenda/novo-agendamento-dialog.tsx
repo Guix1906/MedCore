0e438991560c263ac14c557dfd4ad75cbe2cad64
Guixmusic <testsystemsgui@gmail.com>
2026-08-24 09:48:16 -0300

    auto-sync: 2026-08-24 12:48:16

--- src/components/agenda/novo-agendamento-dialog.tsx
+++ src/components/agenda/novo-agendamento-dialog.tsx
@@ -41,6 +41,7 @@ import { useActiveCompany } from "@/hooks/use-active-company";
 import { isUuid, toValidUuid, ensureValidUuid } from "@/lib/uuid";
 import { mergeWithLocalPatients } from "@/lib/local-patients";
 import { saveStoredLocalEvent } from "@/lib/local-events";
+import { getStoredLocalDoctors, saveStoredLocalDoctor } from "@/lib/local-doctors";
 import { useClinicCities } from "@/hooks/use-clinic-cities";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
